2f232ef847afabd9535cf77efcd29c1e509eb284
Lewis Stapleton <lewis.stapleton@outlook.com>
2018-07-06 14:59:04 +0100

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                Console.ReadLine();
+                return;
+            }
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IdentityServer/Config.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            yield return new ApiResource("api1", "My API");
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            yield return new Client()
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedScopes = { "api1" }
+            };
+        }
+    }
+}
