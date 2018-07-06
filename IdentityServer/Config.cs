405c4227e7ca327b4153109aefd8a3aa14c2e37c
Lewis Stapleton <lewis.stapleton@outlook.com>
2018-07-06 16:15:48 +0100

    Add test users, and password grant to IdentityServer

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IdentityServer4.Models;
+using IdentityServer4.Test;
 
 namespace IdentityServer
 {
@@ -21,6 +22,31 @@ namespace IdentityServer
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets = { new Secret("secret".Sha256()) },
                 AllowedScopes = { "api1" }
+            };
+
+            yield return new Client()
+            {
+                ClientId = "ro.client",
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedScopes = { "api1" }
+            };
+        }
+
+        public static IEnumerable<TestUser> GetUsers()
+        {
+            yield return new TestUser()
+            {
+                SubjectId = "1",
+                Username = "alice",
+                Password = "password"
+            };
+
+            yield return new TestUser
+            {
+                SubjectId = "2",
+                Username = "bob",
+                Password = "password"
             };
         }
     }
--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -19,7 +19,8 @@ namespace IdentityServer
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddInMemoryApiResources(Config.GetApiResources())
-                .AddInMemoryClients(Config.GetClients());
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers().ToList());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
