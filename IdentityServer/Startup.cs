7258dc1939ca26ca5f9b71fdb793cf2a86633d93
Lewis Stapleton <lewis.stapleton@outlook.com>
2018-07-09 15:00:32 +0100

    Add default IdentityServer4 UI
    Add the provided Quickstart files to the Identity Server project, to enable OpenID/OAuth flows

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -16,6 +16,8 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddInMemoryApiResources(Config.GetApiResources())
@@ -32,6 +34,9 @@ namespace IdentityServer
             }
 
             app.UseIdentityServer();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
